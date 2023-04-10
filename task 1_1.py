aa944cbcec118e554c7373f5d0168083ba356d9f
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-10 10:23:58 +0300

    Выполнено

--- task 1_1.py
+++ task 1_1.py
@@ -18,3 +18,32 @@ print(my_favorite_songs[25:30] + my_favorite_songs[33:49])
 
 # второй с конца
 print(my_favorite_songs[-26:-15])
+
+# Да, но лучше все таки заставить пайтон искать индекса самостоятельно)
+# Решение с помощью индексации строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('N') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('S') : 
+     my_favorite_songs.find(', A')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('St') : 
+    my_favorite_songs.rfind(', N')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
+
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
